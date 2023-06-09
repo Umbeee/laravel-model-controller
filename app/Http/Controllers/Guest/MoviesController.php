1da5d7efb1c6226e32874b9c2836efb85a4c5fc3
Umbeee <umberto.nesta@hotmail.com>
2023-06-09 16:53:00 +0200

    creazione repo, creato un $movies che con successo mi riporta tutti i dati del db

---
+++ app/Http/Controllers/Guest/MoviesController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\Guest;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Movie;
+
+class MoviesController extends Controller
+{
+    public function getMovies(){
+        
+        
+        $movies = Movie::All();
+        
+        return view('movies', compact ('movies') );
+
+    }
+}
