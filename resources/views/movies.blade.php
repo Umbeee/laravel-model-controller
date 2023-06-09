e421dfe6c034a7c08799af820097af78fbd0583c
Umbeee <umberto.nesta@hotmail.com>
2023-06-09 17:30:08 +0200

    progetto terminato, rimane da spostare navbar a componente indipendente

--- resources/views/movies.blade.php
+++ resources/views/movies.blade.php
@@ -4,20 +4,44 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    @vite('resources/js/app.js')
 </head>
 <body>
+
+    <div class="container bg-light rounded">
+
+
+    <ul class="nav my-5 bg-info rounded">
+    <li class="nav-item">
+        <a class="nav-link active text-dark" href="/">Home</a>
+    </li>
+    <li class="nav-item">
+        <a href="/movies" class="nav-link text-dark">movies</a>
+    </li>
+    </ul>
+
+    <h1>Movies</h1>
+
+
+    <div class="container-cards">
+
+        @foreach( $movies as $elem )
     
-    <h1>pagina movies</h1>
+            <div class="card">
+                <div class="card-body">
+                    <h2>{{ $elem['original_title'] }}</h2>
+                    <h4>{{ $elem['title'] }}</h4>
+                    <h6>Nazionalità: {{ $elem['nationality'] }}</h6>
+                    <h6>Uscita: {{ $elem['date'] }}</h6>
+                    <h6>{{ $elem['vote'] }} / 10</h6>
+                </div>
+            </div>
+        @endforeach
+    </div>
 
-    @foreach( $movies as $elem )
+    </div>
+    
 
-        <div class="card">
-            <h3>{{ $elem['title'] }}</h3>
-        </div>
-
-    @endforeach
-
-    @php  dd($movies)  @endphp
 
 </body>
 </html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -16,8 +16,27 @@
 </head>
 
 <body>
+    <div class="container bg-light rounded">
 
-    <h1>prova</h1>
+
+        <ul class="nav my-5 bg-info rounded">
+          <li class="nav-item">
+            <a class="nav-link active text-dark" href="/">Home</a>
+          </li>
+          <li class="nav-item">
+            <a href="/movies" class="nav-link text-dark">movies</a>
+          </li>
+        </ul>
+
+        <h1>Home page del sito</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam temporibus ut tempora, ea magnam consequatur praesentium consequuntur accusamus labore porro illum voluptate, consectetur voluptatem quod non voluptates earum recusandae? Sunt.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat soluta, repellat beatae ullam velit ex possimus corrupti sit est veniam neque necessitatibus, et aspernatur ea architecto repudiandae blanditiis sequi vitae!
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Qui voluptatum beatae molestiae dolores commodi deserunt alias optio quod animi labore rerum cum natus officiis nulla, expedita illo, incidunt quasi tenetur.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit dolorum sunt quidem sapiente saepe nemo autem distinctio rerum, quia reprehenderit placeat voluptates veniam eligendi, alias quaerat sit! Nam, aliquid alias?
+        </p>
+
+
+    </div>
 
 </body>
 
